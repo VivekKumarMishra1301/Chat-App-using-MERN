fc2d4930a7cece5b7e247b4b30355229dcdbddb7
Vivek <vivekkmishra1301@gmail.com>
2023-10-22 12:07:02 +0530

    Search User SideDrawer

--- Backend/controllers/chatControllers.js
+++ Backend/controllers/chatControllers.js
@@ -4,7 +4,7 @@ const User = require('../Models/user.js');
 const accessChat = asyncHandler(async (req, res) => {
 
     const { userId } = req.body;
-
+    console.log(userId);
     if (!userId) {
         console.log("UserId param not sent with request");
         return res.sendStatus(400);
@@ -15,8 +15,9 @@ const accessChat = asyncHandler(async (req, res) => {
         $and: [
             { users: { $elemMatch: { $eq: req.user._id } } },
             { users: { $elemMatch: { $eq: userId } } },
-        ]
-    }).populate("users", "-password").populate("latestMessage");
+        ],
+    }).populate("users", "-password")
+        .populate("latestMessage");
 
     isChat = await User.populate(isChat, {
         path: 'latestMessage.sender',
@@ -31,23 +32,24 @@ const accessChat = asyncHandler(async (req, res) => {
             isGroupChat: false,
             users: [req.user._id, userId],
         };
+        
+    
+        try {
+            const createChat = await Chat.create(chatData);
+            const fullChat = await Chat.findOne({ _id: createChat._id }).populate(
+                "users",
+                "-password"
+            );
+            res.status(200).send(fullChat);
+        } catch (error) {
+            res.status(400);
+            throw new Error(error.message);
+        }
     }
-
-    try {
-        const createChat = await Chat.create(chatData);
-        const fullChat = await Chat.findOne({ _id: createChat._id }).populate(
-            "users",
-            "-password"
-        );
-        res.status(200).send(fullChat);
-    } catch (error) {
-        res.status(400);
-        throw new Error(error.message);
-    }
-
 });
 const fetchChats = asyncHandler(async (req,res) => {
     try {
+        // console.log(req.user._id);
         Chat.find({ users: { $elemMatch: { $eq: req.user._id } } }).
             populate("users", '-password')
             .populate('groupAdmin', '-password')
--- Frontend/src/components/chatWindow/myChats.jsx
+++ Frontend/src/components/chatWindow/myChats.jsx
@@ -1,8 +1,106 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import {ChatState} from '../../context/chatProvider';
+import { useToast } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons';
+import {} from '../chatConfig/chatLogics'
+import { getSender } from '../chatConfig/chatLogics';
+import { Text } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import axios from 'axios';
 const myChats = () => {
+  const [loggedUser, setLoggedUSer] = useState();
+  const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
+  const toast = useToast();
+  const fetchChats = async () => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          
+        },
+      };
+      const { data } = await axios.get('http://localhost:5000/api/chat', config)
+      setChats(data);
+    } catch (error) {
+      toast({
+        title: 'Error Occured',
+        description: 'Failed to Load the Chats',
+        status: 'error',
+        duration: 5000,
+        isCLosable: true,
+        position:'bottom-left',
+      })
+    }
+  }
+  useEffect(() => {
+    setLoggedUSer(JSON.parse(localStorage.getItem('userInfo')));
+    fetchChats();
+  },[])
   return (
-    <div>myChats</div>
+    <Box
+      display={{base:selectedChat?"none":"flex",md:"flex"}}
+      flexDirection='column'
+      alignItems='center'
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "28px", md: "30px" }}
+        fontFamily="Work sans"
+        display="flex"
+        width="100%"
+        justifyContent="space-between"
+        alignItems="center"
+
+      >
+        My Chats
+        <Button
+          display="flex"
+          fontSize={{ base: "17px", md: "10px", lg: "10px" }}
+          rightIcon={<AddIcon/>}
+        >
+          New Group Chat
+        </Button>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        p={3}
+        bg="#F8F8F8"
+        w='100%'
+        h='100%'
+        borderRadius='lg'
+        overflowY='hidden'
+      >
+        {chats ? (
+          <Stack overflowY='scroll'>
+            {chats.map((chat) => (
+              <Box
+                onClick={() => setSelectedChat(chat)}
+                cursor="pointer"
+                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                color={selectedChat === chat ? "white" : "black"}
+                px={3}
+                py={2}
+                borderRadius="lg"
+                key={chat._id}
+              >
+                <Text>
+                  {!chat.isGroupChat?(getSender(loggedUser,chat.users)):(chat.chatName)}
+                </Text>
+              </Box>
+            ))}
+          </Stack>
+        ):(<chatLoading/>)}
+      </Box>
+    </Box>
   )
 }
 
