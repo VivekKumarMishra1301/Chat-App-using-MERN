494671a60ed47ff458f18788cedca45165af756f
Vivek <vivekkmishra1301@gmail.com>
2023-10-30 20:01:37 +0530

    created Ui for Messages sent and Recieved

--- Frontend/src/components/chatWindow/scrollableChat.jsx
+++ Frontend/src/components/chatWindow/scrollableChat.jsx
@@ -1,42 +1,50 @@
 import React from 'react'
 import ScrollableFeed from 'react-scrollable-feed'
-import { isSameSender,isLastMessage } from '../chatConfig/chatLogics'
+import { isSameSender,isLastMessage,isSameSenderMargin,isSameUser } from '../chatConfig/chatLogics'
 import { ChatState } from '../../context/chatProvider'
 import { Avatar, Tooltip } from '@chakra-ui/react'
 
 
 const scrollableChat = ({messages}) => {
     const { user } = ChatState();
-    console.log('done')
+  
+  // console.log(messages);
     return (
         <ScrollableFeed>
-            {messages &&
-                messages.map((m, i) => (
-                <div
-                    style={{ display: 'flex' }}
-                    key={m._id}
-                >
-                        {(isSameSender(messages, m, i, user._id))
-                            || isLastMessage(messages, m.i, user._id)
-                        && <Tooltip
-                                lable={m.sender.name}
-                                placement='bottom-start'
-                                hasArrow
-                            >
-                                <Avatar
-                                    mt='7px'
-                                    mr={1}
-                                    size='sm'
-                                    cursor='pointer'
-                                    name={m.sender.name}
-                                    src={m.sender.pic}
-                                >
-                                        
-                                </Avatar>
-                            </Tooltip>}
-                </div>
-            ))}
-        </ScrollableFeed>
+      {messages &&
+        messages.map((m, i) => (
+          <div style={{ display: "flex" }} key={m._id}>
+            {(isSameSender(messages, m, i, user._id) ||
+              isLastMessage(messages, i, user._id)) && (
+                // m.sender.name
+              <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                <Avatar
+                  mt="7px"
+                  mr={1}
+                  size="sm"
+                  cursor="pointer"
+                  name={m.sender.name}
+                  src={m.sender.pic}
+                />
+              </Tooltip>
+            )}
+            <span
+              style={{
+                backgroundColor: `${
+                  m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                }`,
+                marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+                borderRadius: "20px",
+                padding: "5px 15px",
+                maxWidth: "75%",
+              }}
+            >
+              {m.content}
+            </span>
+          </div>
+        ))}
+    </ScrollableFeed>
     );
 }
 
